b147e8d51dff2a3e7657b1290ef3fa13d44cee42
Ananth <krishna.ananth3@gmail.com>
2018-12-21 12:17:26 -0500

    finished RMSE, EKF and KF

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -25,7 +25,6 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 void KalmanFilter::Predict() {
   
   // State prediction using KF equations
-
   x_ = F_ * x_;
   MatrixXd Ft = F_.transpose();
   P_ = F_ * P_ * Ft + Q_;
@@ -50,7 +49,31 @@ void KalmanFilter::Update(const VectorXd &z) {
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+  
+  // Calcualte EKF gain
+  double px = x_(1);
+  double py = x_(2);
+  double vx = x_(3);
+  double vy = x_(4);
+  double h1 = sqrt(px*px + py*py);
+  double h2 = atan2(py,px);
+  double h3 = (px*vx + py*vy)/h1;
+
+  VectorXd z_pred;
+  z_pred << h1,
+            h2,
+            h3;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //New estimated positions
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size,x_size);
+  P_ = (I - K * H_) * P_;
+
 }
